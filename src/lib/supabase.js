27abf9b9efbae3086e6fe113625e099c3d34dcce
Gaurav <gaurav28.official@gmail.com>
2025-09-05 23:45:34 +0530

    updated superbase project

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Initialize Supabase client
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://btlgquarcynurgrrhzhs.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0bGdxdWFyY3ludXJncnJoemhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3Mjg3MzIsImV4cCI6MjA1ODMwNDczMn0.hAhyY8s0vKc8G5FEVAwB8U1JeS36lOJphXP2YvoBr1c';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://wxclfwyhdmssgflehjfo.supabase.co";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4Y2xmd3loZG1zc2dmbGVoamZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4Mzc1MTYsImV4cCI6MjA3MjQxMzUxNn0.l2A38qTc9QPB1p18qPGYa32L-s_1tOosMQ_m4Xk4KpI";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
